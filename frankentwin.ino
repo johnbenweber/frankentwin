7918e2bdd8b150a6749168941a1a6e84ac6e24f2
John Weber <johnbenweber@gmail.com>
2024-07-13 12:35:01 -0400

    Fully functional prototype.

--- frankentwin.ino
+++ frankentwin.ino
@@ -1,4 +1,4 @@
-#include <Wire.h>
+ #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 #include <msTask.h>
 #include <Bounce2.h>
@@ -7,231 +7,257 @@
 LiquidCrystal_I2C lcd(0x27, 16, 2); // Set the LCD address to 0x27 for a 16 chars and 2 line display
 
 // Pin definitions
-const int buttonStartEvent1 = 3;
-const int buttonStartEvent2 = 2;
-const int buttonProgrammingMode = 4;
-const int buttonIncreaseEvent1 = 5;
-const int buttonDecreaseEvent1 = 6;
-const int buttonIncreaseEvent2 = 7;
-const int buttonDecreaseEvent2 = 8;
-const int ledEvent1 = 13;
-const int ledEvent2 = 13;
+const int pinSwitch1 = 2;
+const int pinSwitch2 = 3;
+const int pinProgMode = 4;
+const int pin1Up = 5;
+const int pin1Dn = 6;
+const int pin2Up = 7;
+const int pin2Dn = 8; 
+const int pinOutput1 = 11;
+const int pinOutput2 = 12;
 
-Bounce2::Button debouncer1 = Bounce2::Button();
-Bounce2::Button debouncer2 = Bounce2::Button();
-Bounce2::Button programDebouncer = Bounce2::Button();
-Bounce2::Button timer1UpDebouncer = Bounce2::Button();
-Bounce2::Button timer1DownDebouncer = Bounce2::Button();
-Bounce2::Button timer2UpDebouncer = Bounce2::Button();
-Bounce2::Button timer2DownDebouncer = Bounce2::Button();
+Bounce2::Button buttonSwitch1 = Bounce2::Button();
+Bounce2::Button buttonSwitch2 = Bounce2::Button();
+Bounce2::Button buttonProgMode = Bounce2::Button();
+Bounce2::Button button1Up = Bounce2::Button();
+Bounce2::Button button1Dn = Bounce2::Button();
+Bounce2::Button button2Up = Bounce2::Button();
+Bounce2::Button button2Dn = Bounce2::Button();
 
 // Event durations (in milliseconds)
-unsigned long durationEvent1;
-unsigned long durationEvent2;
 unsigned long defaultDuration = 5000;
 unsigned long maxDuration = 99900;
 
-unsigned long event1Start = 0;
-unsigned long event2Start = 0;
+unsigned long output1Duration;
+unsigned long output2Duration;
 
-unsigned long remaining1 = 0;
-unsigned long remaining2 = 0;
+unsigned long output1Start = 0;
+unsigned long output2Start = 0;
+
+unsigned long output1Remaining = 0;
+unsigned long output2Remaining = 0;
 
 // State variables
-volatile bool event1Active = false;
-volatile bool event2Active = false;
-bool event1ActiveTemp = false;
-bool event2ActiveTemp = false;
+bool output1Active = false;
+bool output2Active = false;
+bool progMode = false;
+int output1Mode = 0;
+int output2Mode = 0;
+int output1ModeDisp = 0;
+int output2ModeDisp = 0;
 
-bool programmingMode = false;
-bool timer1ContinuousMode = false;
-bool timer2ContinuousMode = false;
+void stopOutput1();
+void stopOutput2();
 
-void stopEvent1();
-void stopEvent2();
-
-msTask task1(durationEvent1, stopEvent1);
-msTask task2(durationEvent2, stopEvent2);
+msTask timer1Task(defaultDuration, stopOutput1);
+msTask timer2Task(defaultDuration, stopOutput2);
 
 void setup() {
-  debouncer1.attach(buttonStartEvent1,INPUT_PULLUP);
-  debouncer2.attach(buttonStartEvent2,INPUT_PULLUP);
-  programDebouncer.attach(buttonProgrammingMode,INPUT_PULLUP);
-  timer1UpDebouncer.attach(buttonIncreaseEvent1,INPUT_PULLUP);
-  timer1DownDebouncer.attach(buttonDecreaseEvent1,INPUT_PULLUP);
-  timer2UpDebouncer.attach(buttonIncreaseEvent2,INPUT_PULLUP);
-  timer2DownDebouncer.attach(buttonDecreaseEvent2,INPUT_PULLUP);
+  pinMode(pinOutput1, OUTPUT);
+  pinMode(pinOutput2, OUTPUT);  
   
-  debouncer1.interval(25);
-  debouncer2.interval(25);
-  programDebouncer.interval(25);
-  timer1UpDebouncer.interval(25);
-  timer1DownDebouncer.interval(25);
-  timer2UpDebouncer.interval(25);
-  timer2DownDebouncer.interval(25);
+  buttonSwitch1.attach(pinSwitch1,INPUT_PULLUP);
+  buttonSwitch2.attach(pinSwitch2,INPUT_PULLUP);
+  buttonProgMode.attach(pinProgMode,INPUT_PULLUP);
+  button1Up.attach(pin1Up,INPUT_PULLUP);
+  button1Dn.attach(pin1Dn,INPUT_PULLUP);
+  button2Up.attach(pin2Up,INPUT_PULLUP);
+  button2Dn.attach(pin2Dn,INPUT_PULLUP);
   
-  debouncer1.setPressedState(LOW); 
-  debouncer2.setPressedState(LOW);
-  programDebouncer.setPressedState(LOW);
-  timer1UpDebouncer.setPressedState(LOW);
-  timer1DownDebouncer.setPressedState(LOW);
-  timer2UpDebouncer.setPressedState(LOW);
-  timer2DownDebouncer.setPressedState(LOW);
+  buttonSwitch1.interval(25);
+  buttonSwitch2.interval(25);
+  buttonProgMode.interval(25);
+  button1Up.interval(25);
+  button1Dn.interval(25);
+  button2Up.interval(25);
+  button2Dn.interval(25);
+  
+  buttonSwitch1.setPressedState(LOW); 
+  buttonSwitch2.setPressedState(LOW);
+  buttonProgMode.setPressedState(LOW);
+  button1Up.setPressedState(LOW);
+  button1Dn.setPressedState(LOW);
+  button2Up.setPressedState(LOW);
+  button2Dn.setPressedState(LOW);
 
-  EEPROM.get(0, durationEvent1);
-  EEPROM.get(sizeof(durationEvent1), durationEvent2);
+  EEPROM.get(0, output1Duration);
+  EEPROM.get(sizeof(output1Duration), output2Duration);
+  EEPROM.get(sizeof(output1Duration)+sizeof(output2Duration), output1Mode);
+  EEPROM.get(sizeof(output1Duration)+sizeof(output2Duration)+sizeof(output1Mode), output2Mode);
 
-  if ((durationEvent1 < 0) || (durationEvent1 > maxDuration)) {
-    durationEvent1 = defaultDuration;
+  if ((output1Duration < 0) || (output1Duration > maxDuration)) {
+    output1Duration = defaultDuration;
   }
   
-  if ((durationEvent2 < 0) || (durationEvent2 > maxDuration)) {
-    durationEvent2 = defaultDuration;
-  }  
+  if ((output2Duration < 0) || (output2Duration > maxDuration)) {
+    output2Duration = defaultDuration;
+  }
+
+  if ((output1Mode < 0) || (output1Mode > 2)) {
+    output1Mode = 0;
+  }
+  
+  if ((output2Mode < 0) || (output2Mode > 2)) {
+    output2Mode = 0;
+  }
+
 
   msTask::init();
+  timer1Task.start();
+  timer2Task.start();
   
   lcd.init(); // Initialize the LCD
   lcd.backlight();
 
-  updateLCD(programmingMode, durationEvent1, durationEvent2); // Update the LCD in each loop iteration
+  updateLCD(progMode, output1Mode, output1Duration, output2Mode, output2Duration); // Update the LCD in each loop iteration
 }
 
 void loop() {
-  debouncer1.update();
-  debouncer2.update();
+  buttonSwitch1.update();
+  buttonSwitch2.update();
 
-  // Event 1 start
-  if (debouncer1.pressed() && !event1ActiveTemp && !programmingMode) {
-      event1Active = true;
-      if (timer1ContinuousMode) {
-        startEvent1(event1Start, maxDuration+1);
-      } else {
-        startEvent1(event1Start, durationEvent1);
-      }
-  } else if (debouncer1.released() && event1Active && timer1ContinuousMode) {
-    stopEvent1();
+  output1Active = digitalRead(pinOutput1);
+  output2Active = digitalRead(pinOutput2);
+
+  // Output 1 start
+  if (buttonSwitch1.pressed() && !output1Active && !progMode) {
+    output1Start = millis();
+    digitalWrite(pinOutput1, HIGH);
+    if (output1Mode == 0) { // Timed mode
+      timer1Task.setPeriod(output1Duration);
+      timer1Task.start();
+    } else {
+      timer1Task.setPeriod(maxDuration);
+      timer1Task.start();
+    }
+  } else if (buttonSwitch1.released() && output1Active && output1Mode == 1) {
+    stopOutput1();
+  } else if (buttonSwitch1.pressed() && output1Active && output1Mode == 2) {
+    stopOutput1();
   }
 
-  // Event 2 start
-  if (debouncer2.pressed() && !event2ActiveTemp && !programmingMode) {
-      event2Active = true;
-      if (timer2ContinuousMode) {
-        startEvent2(event2Start, maxDuration+1);
-      } else {
-        startEvent2(event2Start, durationEvent2);
-      }
-  } else if (debouncer2.released() && event2Active && timer2ContinuousMode) {
-    stopEvent2();
+  // Output 2 start
+  if (buttonSwitch2.pressed() && !output2Active && !progMode) {
+    output2Start = millis();
+    digitalWrite(pinOutput2, HIGH);
+    if (output2Mode == 0) { // Timed mode
+      timer2Task.setPeriod(output2Duration);
+      timer2Task.start();
+    } else {
+      timer2Task.setPeriod(maxDuration);
+      timer2Task.start();
+    }
+  } else if (buttonSwitch2.released() && output2Active && output2Mode == 1) {
+    stopOutput2();
+  } else if (buttonSwitch2.pressed() && output2Active && output2Mode == 2) {
+    stopOutput2();
   }
-
-  noInterrupts();
-  event1ActiveTemp = event1Active;
-  event2ActiveTemp = event2Active;
-  interrupts();
   
-  timer1UpDebouncer.update();
-  timer1DownDebouncer.update();
-  timer2UpDebouncer.update();
-  timer2DownDebouncer.update();
-  programDebouncer.update();
+  button1Up.update();
+  button1Dn.update();
+  button2Up.update();
+  button2Dn.update();
+  buttonProgMode.update();
   
   // Toggle programming mode
-  if (programDebouncer.pressed() && event1ActiveTemp == false && event2ActiveTemp == false) {
-      programmingMode = !programmingMode;
-
-      EEPROM.put(0, durationEvent1);
-      EEPROM.put(sizeof(durationEvent1), durationEvent2); 
+  if (buttonProgMode.pressed() && !output1Active && !output2Active) {
+    progMode = !progMode;
+    EEPROM.put(0, output1Duration);
+    EEPROM.put(sizeof(output1Duration), output2Duration); 
   }
 
   // Toggle continuous mode
-  if (timer1UpDebouncer.pressed() && event1ActiveTemp == false && programmingMode == false) {
-    timer1ContinuousMode = true;
-  } else if (timer1DownDebouncer.pressed() && event1ActiveTemp == false && programmingMode == false) {
-    timer1ContinuousMode = false;
+  if (button1Up.pressed() && !output1Active && !progMode) {
+    if (output1Mode == 1) {
+      output1Mode = 2;
+    } else {
+      output1Mode = 1;
+    }
+  } else if (button1Dn.pressed() && !output1Active && !progMode) {
+    output1Mode = 0;
   }
 
   // Toggle continuous mode
-  if (timer2UpDebouncer.pressed() && event2ActiveTemp == false && programmingMode == false) {
-    timer2ContinuousMode = true;
-  } else if (timer2DownDebouncer.pressed() && event2ActiveTemp == false && programmingMode == false) {
-    timer2ContinuousMode = false;
+  if (button2Up.pressed() && !output2Active && !progMode) {
+    if (output2Mode == 1) {
+      output2Mode = 2;
+    } else {
+      output2Mode = 1;
+    }
+  } else if (button2Dn.pressed() && !output2Active && !progMode) {
+    output2Mode = 0;
   }
+
+  EEPROM.put(sizeof(output1Duration)+sizeof(output2Duration), output1Mode);
+  EEPROM.put(sizeof(output1Duration)+sizeof(output2Duration)+sizeof(output1Mode), output2Mode);
 
   // Only adjust durations in programming mode
-  if (programmingMode) {
-    timer1ContinuousMode = false;
-    timer2ContinuousMode = false;
-    if (timer1UpDebouncer.pressed()) {
-      adjustDuration(durationEvent1, 100);
-    } else if (timer1DownDebouncer.pressed()) {
-      adjustDuration(durationEvent1, -100);
+  if (progMode) {
+    if (button1Up.pressed()) {
+      adjustDuration(output1Duration, 100);
+    } else if ((button1Up.read() == LOW) && (button1Up.currentDuration() > 1000) && (millis() % 100 == 0)) {
+        adjustDuration(output1Duration, 250);
+    } else if (button1Dn.pressed()) {
+      adjustDuration(output1Duration, -100);
+    } else if ((button1Dn.read() == LOW) && (button1Dn.currentDuration() > 1000) && (millis() % 100 == 0)) {
+      adjustDuration(output1Duration, -250);
     }
 
-    if (timer2UpDebouncer.pressed()) {
-      adjustDuration(durationEvent2, 100);
-    } else if (timer2DownDebouncer.pressed()) {
-      adjustDuration(durationEvent2, -100);
+    if (button2Up.pressed()) {
+      adjustDuration(output2Duration, 100);
+    } else if ((button2Up.read() == LOW) && (button2Up.currentDuration() > 1000) && (millis() % 100 == 0)) {
+      adjustDuration(output2Duration, 250);
+    } else if (button2Dn.pressed()) {
+      adjustDuration(output2Duration, -100);
+    } else if ((button2Dn.read() == LOW) && (button2Dn.currentDuration() > 1000) && (millis() % 100 == 0)) {
+      adjustDuration(output2Duration, -250);
     }
   }
 
-  if (event1ActiveTemp && !timer1ContinuousMode) {
-    remaining1 = durationEvent1-(millis()-event1Start);
-  } else if (timer1ContinuousMode) {
-    remaining1 = maxDuration+1;
-  } else {
-    remaining1 = durationEvent1;
+  if (output1Active && output1Mode == 0) { // Timer mode
+    output1Remaining = output1Duration-(millis()-output1Start);
+  } else { // Idle
+    output1Remaining = output1Duration;
   }
 
-  if (event2ActiveTemp && !timer2ContinuousMode) {
-    remaining2 = durationEvent2-(millis()-event2Start);
-  } else if (timer2ContinuousMode) {
-    remaining2 = maxDuration+1;
-  } else {
-    remaining2 = durationEvent2;
+  if (output2Active && output2Mode == 0) { // Timer mode
+    output2Remaining = output2Duration-(millis()-output2Start);
+  } else { // Idle
+    output2Remaining = output2Duration;
   }
 
   if (millis() % 100 == 0) {
-    updateLCD(programmingMode, remaining1, remaining2); // Update the LCD in each loop iteration
+    if (progMode) {
+      output1ModeDisp = 0;
+      output2ModeDisp = 0;
+    } else {
+      output1ModeDisp = output1Mode;
+      output2ModeDisp = output2Mode;
+    }
+    updateLCD(progMode, output1ModeDisp, output1Remaining, output2ModeDisp, output2Remaining); // Update the LCD in each loop iteration
+  }
+}
+ 
+void adjustDuration(unsigned long &eventDur, int change) { 
+  if (eventDur+change < 0){
+    eventDur = maxDuration;
+  } else if (eventDur+change > maxDuration) {
+    eventDur = 0;
+  } else {
+    eventDur = eventDur+change;
   }
 }
 
-void adjustDuration(unsigned long &eventDur, int change) {
-  eventDur = max(0, min(eventDur+change, maxDuration)); // Ensure minimum 0
+void stopOutput1() {
+  digitalWrite(pinOutput1, LOW);
 }
 
-void startEvent1(unsigned long &e1Start, unsigned long durEvent1) {
-  e1Start = millis();
-  digitalWrite(ledEvent1, HIGH);
-  if (durEvent1 <= maxDuration) {
-    task1.setPeriod(durEvent1);
-    task1.start();
-  }
+void stopOutput2() {
+  digitalWrite(pinOutput2, LOW);
 }
 
-void stopEvent1() {
-  event1Active = false;
-  digitalWrite(ledEvent1, LOW);
-  task1.stop();
-}
-
-void startEvent2(unsigned long &e2Start, unsigned long durEvent2) {
-  e2Start = millis();
-  digitalWrite(ledEvent2, HIGH);
-  if (durEvent2 <= maxDuration) {
-    task2.setPeriod(durEvent2);
-    task2.start();
-  }
-}
-
-void stopEvent2() {
-  event2Active = false;
-  digitalWrite(ledEvent2, LOW);
-  task2.stop();
-}
-
-void updateLCD(bool progMode, unsigned long rem1, unsigned long rem2) {
-    
+void updateLCD(bool progMode, int mode1, unsigned long rem1, int mode2, unsigned long rem2) {
   lcd.setCursor(6, 0); // Set cursor to middle of the top line
   if (progMode) {
       lcd.print("PROG");
@@ -244,20 +270,24 @@ void updateLCD(bool progMode, unsigned long rem1, unsigned long rem2) {
   
   // Display Timer 1 status
   lcd.setCursor(0, 1); // Bottom left corner
-  if (rem1 <= maxDuration) {
+  if (mode1 == 0 && rem1 <= maxDuration) {
     formatTime(time1, rem1);
     lcd.print(time1);
-  } else {
-    lcd.print("CONT.");
+  } else if (mode1 == 1) {
+    lcd.print(" CONT");
+  } else if (mode1 == 2) {
+    lcd.print(" HOLD");
   }
 
   // Display Timer 2 status
   lcd.setCursor(11, 1); // Bottom right corner
-  if (rem2 <= maxDuration) {
+  if (mode2 == 0 && rem2 <= maxDuration) {
     formatTime(time2, rem2);
     lcd.print(time2);
-  } else {
-    lcd.print("CONT.");
+  } else if (mode2 == 1) {
+    lcd.print(" CONT");
+  } else if (mode2 == 2) {
+    lcd.print(" HOLD");
   }
 }
 
